942ca87189238acb9e5ea5f42c3281bd1e271170
PierceZhang <pzfunky@outlook.com>
2021-02-24 14:07:17 +0800

    删除多余不用的东西  增加首页背景图片

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">登 录</h3>
       </div>
 
       <el-form-item prop="username">
@@ -180,7 +180,8 @@ $light_gray:#eee;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  background-image: url(~@/assets/vaio_2013_dark_1920x1080.jpg);
+  background-size: cover;
   overflow: hidden;
 
   .login-form {
