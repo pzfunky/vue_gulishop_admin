b7eacf21ed59cf4ac1f6aefa94884d59f3556d42
PierceZhang <pzfunky@outlook.com>
2021-02-25 18:46:49 +0800

    备份一份用来练习

---
+++ src0/views/product/attr/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      AttrList
+  </div>
+</template>
+
+<script>
+export default {
+    name:'AttrList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src0/views/product/spu/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      SpuList
+  </div>
+</template>
+
+<script>
+export default {
+    name:'SpuList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
