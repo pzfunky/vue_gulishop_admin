d935e230f3e319339ea281e0f1fb700b8f5bd74d
pzfunky <pzfunky@outlook.com>
2021-03-01 02:23:35 +0800

    day03更新完成练习

--- src0/components/CategorySelector/index.vue
+++ src0/components/CategorySelector/index.vue
@@ -3,23 +3,20 @@
         <el-form :inline="true" class="demo-form-inline" :model="cForm">
 
             <el-form-item label="一级分类">
-                <el-select placeholder="请选择">
-                    <el-option label="区域一" value="shanghai"></el-option>
-                    <el-option label="区域二" value="beijing"></el-option>
+                <el-select placeholder="请选择" v-model="cForm.category1Id" @change="handlerCategory1">
+                    <el-option :label="c1.name" :value="c1.id" v-for="(c1, index) in category1List" :key="c1.id"></el-option>
                 </el-select>
             </el-form-item>
 
             <el-form-item label="二级分类">
-                <el-select placeholder="请选择">
-                    <el-option label="区域一" value="shanghai"></el-option>
-                    <el-option label="区域二" value="beijing"></el-option>
+                <el-select placeholder="请选择" v-model="cForm.category2Id" @change="handlerCategory2">
+                    <el-option :label="c2.name" :value="c2.id" v-for="(c2, index) in category2List" :key="c2.id"></el-option>
                 </el-select>
             </el-form-item>
 
             <el-form-item label="三级分类">
-                <el-select placeholder="请选择">
-                    <el-option label="区域一" value="shanghai"></el-option>
-                    <el-option label="区域二" value="beijing"></el-option>
+                <el-select placeholder="请选择" v-model="cForm.category3Id" @change="handlerCategory3">
+                    <el-option :label="c3.name" :value="c3.id" v-for="(c3, index) in category3List" :key="c3.id"></el-option>
                 </el-select>
             </el-form-item>
 
@@ -40,6 +37,55 @@ export default {
                 category2Id:'',
                 category3Id:'',
             }
+        }
+    },
+    mounted(){
+        this.getCategory1List()
+    },
+    methods:{
+        //获取一级分类列表
+        async getCategory1List(){
+            let result = await this.$API.category.getCategory1()
+            if(result.code === 200){
+                this.category1List = result.data
+            }
+        },
+        //获取二级分类列表
+        async getCategory2List(category1Id){
+            let result = await this.$API.category.getCategory2(category1Id)
+            if(result.code === 200){
+                this.category2List = result.data
+            }
+        },
+        //获取三级分类列表
+        async getCategory3List(category2Id){
+            let result = await this.$API.category.getCategory3(category2Id)
+            if(result.code === 200){
+                this.category3List = result.data
+            }
+        },
+        //选中一级分类的回调,获取二级分类
+        async handlerCategory1(category1Id){
+            //用户选中一级分类,需要清空二级三级分类,要触发自定义事件把1级id传给父组件
+            this.category2List = []
+            this.category3List = []
+            this.cForm.category2Id = ''
+            this.cForm.category3Id = ''
+            this.getCategory2List(category1Id)
+            this.$emit('handlerCategory',{categoryId:category1Id,level:1})
+        },
+        //选中二级分类的回调,获取三级分类
+        async handlerCategory2(category2Id){
+            //用户选中二级分类,需要清空二级三级分类,要触发自定义事件把2级id传给父组件
+            this.category3List = []
+            this.cForm.category3Id = ''
+            this.getCategory3List(category2Id)
+            this.$emit('handlerCategory',{categoryId:category2Id,level:2})
+        },
+        //选中三级分类的回调
+        async handlerCategory3(category3Id){
+            //用户选中三级分类,要触发自定义事件把3级id传给父组件
+            this.$emit('handlerCategory',{categoryId:category3Id,level:3})
         }
     }
 }
--- src0/views/product/attr/List.vue
+++ src0/views/product/attr/List.vue
@@ -1,18 +1,176 @@
 <template>
   <div>
+    <!-- 三级联动 -->
     <el-card>
-      <CategorySelector/>      
+      <CategorySelector @handlerCategory="handlerCategory"/>      
     </el-card>
 
+    <!-- 属性列表 -->
     <el-card style="margin-top:20px">
+      <div v-show="isShowList">
+        <!-- 添加属性按钮 -->
+        <el-button type="primary" @click="showAddDiv" :disabled="!category3Id">添加属性</el-button>
+        <!-- 属性列表 -->
+        <el-table
+          :data="attrList"
+          border
+          style="width: 100%;margin:20px 0">
 
+          <el-table-column
+            type="index"
+            label="序号"
+            width="80"
+            align="center">
+          </el-table-column>
+
+          <el-table-column
+            prop="attrName"
+            label="属性名"
+            width="150">
+          </el-table-column>
+
+          <el-table-column
+            prop="attrValueList"
+            label="属性值列表"
+            width="width">
+            <template slot-scope="{row,$index}">
+              <el-tag type="success" v-for="attrValue in row.attrValueList" :key="attrValue.id">{{attrValue.valueName}}</el-tag>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            prop="prop"
+            label="操作"
+            width="150">
+            <template slot-scope="{row,$index}">
+              <HintButton type="waring" icon="el-icon-edit" size="mini" title="修改" @click="showUpdateDiv(row)"/>
+              <HintButton type="danger" icon="el-icon-delete" size="mini" title="删除"></HintButton>
+            </template>
+          </el-table-column>
+
+        </el-table>
+      </div>
+
+      <div v-show="!isShowList">
+        <el-form :inline='true' :model="attrForm">
+          <el-form-item label="属性名">
+            <el-input placeholder="请输入属性名" v-model="attrForm.attrName"></el-input>
+          </el-form-item>
+        </el-form>
+
+        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue">添加属性值</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
+
+        <el-table
+          :data="attrForm.attrValueList"
+          border
+          style="width: 100%;margin:20px 0">
+
+          <el-table-column
+            type="index"
+            label="序号"
+            width="80"
+            align="center">
+          </el-table-column>
+
+          <el-table-column
+            prop="prop"
+            label="属性值名称"
+            width="width">
+            <template slot-scope="{row,$index}">
+              <el-input placeholder="请输入属性值名称" v-model="row.valueName"></el-input>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            prop="prop"
+            label="操作"
+            width="width">
+            <template slot-scope="{row,$index}">
+              <HintButton type="danger" icon="el-icon-delete" size="mini" title="删除"></HintButton>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <el-button type="primary" icon="el-icon-edit">保存</el-button>
+        <el-button @click="isShowList = true" >取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import cloneDeep from 'lodash/cloneDeep'
 export default {
-    name:'AttrList'
+    name:'AttrList',
+    data(){
+      return {
+        category1Id:'',
+        category2Id:'',
+        category3Id:'',
+        attrList:[],  //商品属性列表
+        attrForm:{    //待修改的商品对象
+          attrName:'',  //属性名称
+          attrValueList:[],
+          categoryId:0, //data中不能使用this来获取data中的另一个值 data在初始化时谁先谁后不一定
+          categoryLevel:3
+        },
+        isShowList:true    //标识属性列表是否显示
+      }
+    },
+    methods:{
+      //自定义事件的回调
+      handlerCategory({categoryId,level}){
+        if(level === 1){
+          this.category1Id = categoryId
+          //子组件重新选择一级,清空二三级id和属性列表
+          this.category2Id = ''
+          this.category3Id = ''
+          this.attrList = []
+        }else if(level === 2){
+          this.category2Id = categoryId
+          //子组件重新选择一级,清空二三级id和属性列表
+          this.category3Id = ''
+          this.attrList = []
+        }else if(level === 3){
+          this.category3Id = categoryId
+          //发请求拿属性列表
+          this.getAttrList()
+        }
+        this.isShowList = true
+      },
+      //请求获取属性
+      async getAttrList(){
+        let {category1Id,category2Id,category3Id} = this
+        let result = await this.$API.attr.getList(category1Id,category2Id,category3Id)
+        if(result.code === 200){
+          this.attrList = result.data
+        }
+      },
+      //重置attrForm
+      resetAttrForm(){
+        this.attrForm = {    //待修改的商品对象
+          attrName:'',  //属性名称
+          attrValueList:[],
+          categoryId:0, //data中不能使用this来获取data中的另一个值 data在初始化时谁先谁后不一定
+          categoryLevel:3
+        }
+      },
+      //点击添加属性的回调 显示添加属性对话框
+      showAddDiv(){
+        this.isShowList = false
+        this.resetAttrForm()
+      },
+      //点击编辑显示属性编辑对话框
+      showUpdateDiv(row){
+        this.isShowList = false
+        this.attrForm = cloneDeep(row)
+      },
+      //点击添加属性值的回调
+      addAttrValue(){
+        this.attrForm.attrValueList.push({})
+      }
+    }
 }
 </script>
 
